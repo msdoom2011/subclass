d0d147349b738d7ed0a8a9f209c6b5837182424a
msdoom <msdoom2011@gmail.com>
2015-02-24 21:07:00 +0200

    Fixed bug

--- src/Tools/LoadingTools.js
+++ src/Tools/LoadingTools.js
@@ -48,7 +48,6 @@ Subclass.Tools.LoadingTools = (function()
 
             xmlhttp.onreadystatechange = function() {
                 if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                    //console.log(fileName);
                     var script = document.createElement('script');
                     script.setAttribute("type", "text/javascript");
                     script.text = xmlhttp.responseText;
